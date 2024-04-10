35cd7834b0e62f2ca9e8fb553524b7731a23ae2b
ilyapatis24 <ilyapatis24@mail.ru>
2024-04-11 00:42:33 +0300

    second mission is completed

---
+++ 02/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+
+class Big_integer
+{
+private:
+	std::string num;
+
+public:
+	Big_integer(std::string value) : num(value) {}
+
+	Big_integer(Big_integer&& other) noexcept : num(std::exchange(num, other.num)) {}
+
+	Big_integer& operator=(Big_integer&& other) noexcept
+	{
+		std::swap(num, other.num);
+		return *this;
+	}
+
+	int operator+(Big_integer& other)
+	{
+		int number1 = std::stoi(num);
+		int number2 = std::stoi(other.num);
+		int result = number1 + number2;
+
+		return result;
+	}
+
+	long long operator*(Big_integer& other)
+	{
+		int number1 = std::stoi(num);
+		int number2 = std::stoi(other.num);
+		int result = number1 * number2;
+
+		return result;
+	}
+};
+
+int main(int argc, char** argv) {
+    setlocale(LC_ALL, "Russian");
+	auto number1 = Big_integer("114575");
+	auto number2 = Big_integer("78524");
+	auto result = number1 * number2;
+	std::cout << result;
+    return 0;
+}
